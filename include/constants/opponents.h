24558c3441908a95ead5fb2b7ab1df7ab30a4c69
gpansino <gregdawizard@gmail.com>
2024-04-22 14:02:09 -0400

    Bug fix Steven Granite Cave

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -866,7 +866,7 @@
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      856
+#define TRAINERS_COUNT                      857
 #define MAX_TRAINERS_COUNT                  864
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
