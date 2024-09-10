0976825b3764bf30a42a0b1c8b4672138566dcb5
gpansino <gregdawizard@gmail.com>
2024-09-10 00:48:27 -0400

    invis floor fix

--- include/constants/field_effects.h
+++ include/constants/field_effects.h
@@ -71,6 +71,7 @@
 #define FLDEFF_USE_VS_SEEKER             67
 #define FLDEFF_X_ICON                    68
 #define FLDEFF_DOUBLE_EXCL_MARK_ICON     69
+#define FLDEFF_INVIS_FLOOR               70
 
 #define FLDEFFOBJ_SHADOW_S              0
 #define FLDEFFOBJ_SHADOW_M              1
@@ -109,6 +110,8 @@
 #define FLDEFFOBJ_BUBBLES               34
 #define FLDEFFOBJ_SMALL_SPARKLE         35
 #define FLDEFFOBJ_RAYQUAZA              36
+#define FLDEFFOBJ_INVIS_FLOOR           37
+#define FLDEFFOBJ_INVIS_FLOOR_LEAVE     38
 
 #define FLDEFF_PAL_TAG_CUT_GRASS          0x1000
 #define FLDEFF_PAL_TAG_SECRET_POWER_TREE  0x1003
@@ -122,5 +125,6 @@
 #define FLDEFF_PAL_TAG_SMALL_SPARKLE      0x100F
 #define FLDEFF_PAL_TAG_HOF_MONITOR        0x1010
 #define FLDEFF_PAL_TAG_UNKNOWN            0x1011
+#define FLDEFF_PAL_TAG_INVIS_FLOOR        0x1012
 
 #endif // GUARD_FIELD_EFFECT_CONSTANTS_H
